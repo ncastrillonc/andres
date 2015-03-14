a4ab337dcc2f57f7f3db665d8924a1b140eb482c
Andrés Pineda <andr3s2@gmail.com>
2015-03-14 17:34:49 -0500

    Clase 2

--- app/routes.php
+++ app/routes.php
@@ -4,5 +4,10 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+Route::get('/test', function()
+{
+	return View::make();
+});
 
 Route::controller('personal','PersonalController');
+Route::controller('clase','Clase2Controller');
